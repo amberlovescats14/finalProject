c442229800635d52f832856c6d5e183539ae9b7d
Amber Jones <amberlovescats14@gmail.com>
2019-06-13 20:57:17 -0500

    got map on trails

--- final-project/src/components/Trails.js
+++ final-project/src/components/Trails.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+const google = window.google = window.google ? window.google : {}
 
 
 
-export default class Trails extends Component {
+class Trails extends Component {
   constructor(props){
     super(props)
     this.state = {
@@ -14,16 +16,33 @@ export default class Trails extends Component {
   }
 
   render() {
-    console.log("trailDATA:", this.props.trailDATA)
-    console.log("STATE:", this.state.data)
-    
 
-    console.log(this.props.trailDATA)
     return (
       <div>
         <h1>Trails</h1>
         <h2>{this.props.trailHeader}</h2>
+
+        <Map google={this.props.google} zoom={11}
+        initialCenter = {{
+          lat: 29.424122,
+          lng: -98.493629
+        }}
+        onClick={this.onMapClicked}
+        >
+ 
+
+      </Map>
+
+        
+        {/* {this.props.trailDATA.map(item => {
+
+          console.log("IN THE MAP:", item.geometry)
+        })} */}
       </div>
     )
   }
 }
+
+export default GoogleApiWrapper({
+  apiKey: ("")
+})(Trails)
